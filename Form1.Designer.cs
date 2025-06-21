7dfef35979f2eacc8843003b8841e85ac337a510
Andre1206 <h0a4r0r8y@gmail.com>
2025-06-21 19:51:29 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace TextCopier
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.msgTextBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "tree";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(61, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "數論";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(12, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(61, 23);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "圖論";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(12, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(61, 23);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "區間和";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(12, 187);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(61, 23);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Text = "互斥集";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Location = new System.Drawing.Point(12, 298);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(61, 23);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.Text = "常用";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(96, 292);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "print";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // msgTextBox
+            // 
+            this.msgTextBox.Location = new System.Drawing.Point(12, 346);
+            this.msgTextBox.Name = "msgTextBox";
+            this.msgTextBox.Size = new System.Drawing.Size(697, 30);
+            this.msgTextBox.TabIndex = 7;
+            this.msgTextBox.TextChanged += new System.EventHandler(this.msgTextBox_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(177, 292);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 34);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "多測資";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(96, 98);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(192, 34);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "range_query_ST";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(492, 98);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(192, 34);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "range_query_LPST";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(258, 292);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 34);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "board";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Location = new System.Drawing.Point(12, 230);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(61, 23);
+            this.textBox6.TabIndex = 12;
+            this.textBox6.Text = "多重集";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(96, 138);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(192, 34);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "range_query_BIT";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(294, 138);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(192, 34);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "range_query_DBIT";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(96, 181);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(192, 34);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "DS";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(294, 181);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(192, 34);
+            this.button10.TabIndex = 16;
+            this.button10.Text = "DS_map";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(492, 181);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(192, 34);
+            this.button11.TabIndex = 17;
+            this.button11.Text = "DS_vector";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(96, 224);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(192, 34);
+            this.button12.TabIndex = 18;
+            this.button12.Text = "my_multiset";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(96, 6);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 34);
+            this.button13.TabIndex = 19;
+            this.button13.Text = "mint";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(294, 98);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(192, 34);
+            this.button14.TabIndex = 20;
+            this.button14.Text = "range_query_DST";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 406);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.msgTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox msgTextBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextCopier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fileToClipboard(string filePath)
+        {
+            // 檢查檔案是否存在
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    // 讀取檔案所有文字
+                    string text = File.ReadAllText(filePath);
+                    // 將文字複製到剪貼簿
+                    Clipboard.SetText(text);
+                    string fileName = Path.GetFileName(filePath);
+                    // ✅ 成功複製後顯示訊息到 msgTextBox
+                    msgTextBox.Text = $"已成功複製：{fileName}";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("讀取檔案或設定剪貼簿時發生錯誤：" + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("找不到檔案！");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\h0a4r\OneDrive\桌面\code_projects\TextCopier\codeSegments\graph\tree.txt";
+            fileToClipboard(filePath);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\h0a4r\OneDrive\桌面\code_projects\TextCopier\codeSegments\functions\print.txt";
+            fileToClipboard(filePath);
+        }
+
+        private void msgTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\h0a4r\OneDrive\桌面\code_projects\TextCopier\codeSegments\functions\multiTestcases.txt";
+            fileToClipboard(filePath);
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\h0a4r\OneDrive\桌面\code_projects\TextCopier\codeSegments\range_query\range_query_ST.txt";
+            fileToClipboard(filePath);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\h0a4r\OneDrive\桌面\code_projects\TextCopier\codeSegments\range_query\range_query_LPST.txt";
+            fileToClipboard(filePath);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\h0a4r\OneDrive\桌面\code_projects\TextCopier\codeSegments\range_query\range_query_BIT.txt";
+            fileToClipboard(filePath);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\h0a4r\OneDrive\桌面\code_projects\TextCopier\codeSegments\range_query\range_query_DBIT.txt";
+            fileToClipboard(filePath);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\h0a4r\OneDrive\桌面\code_projects\TextCopier\codeSegments\disjoint_set\DS.txt";
+            fileToClipboard(filePath);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\h0a4r\OneDrive\桌面\code_projects\TextCopier\codeSegments\disjoint_set\DS_map.txt";
+            fileToClipboard(filePath);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\h0a4r\OneDrive\桌面\code_projects\TextCopier\codeSegments\disjoint_set\DS_vector.txt";
+            fileToClipboard(filePath);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\h0a4r\OneDrive\桌面\code_projects\TextCopier\codeSegments\my_multiset\my_multiset.txt";
+            fileToClipboard(filePath);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\h0a4r\OneDrive\桌面\code_projects\TextCopier\codeSegments\number_theory\mint.txt";
+            fileToClipboard(filePath);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\h0a4r\OneDrive\桌面\code_projects\TextCopier\codeSegments\functions\board.txt";
+            fileToClipboard(filePath);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\h0a4r\OneDrive\桌面\code_projects\TextCopier\codeSegments\range_query\range_query_DST.txt";
+            fileToClipboard(filePath);
+        }
+    }
+}
